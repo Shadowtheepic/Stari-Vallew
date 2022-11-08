5832a9fa71f05aa2c8a73566a7cdcc3f4c83f5df
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-11-08 09:47:19 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+
+
+
+class Game:
+    def__init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 800))
+        self.clock = pygame.time.Clock()
+    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                dt = self.clocl.tick()/1000
+                pygame.display.update()
+
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
