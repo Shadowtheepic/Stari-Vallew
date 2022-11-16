daec72161bbd8999dad9fefdd5d40dad5999abb9
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-11-16 09:56:07 -0700

    Update level.py

--- level.py
+++ level.py
@@ -7,13 +7,14 @@ class Level:
     def __init__(self):
         self.all_sprites = pygame.sprite.Group()
         
+        
         self.setup()
     
     def setup(self):
         self.player = Player((640, 360), self.all_sprites)
         
     def run(self, dt):
-        print("run game")
+        #print("run game")
         self.display_surface = pygame.display.get_surface()
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
