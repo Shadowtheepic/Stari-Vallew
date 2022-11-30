e03169eecbbb50296bca43f3cce96d3e432ea243
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-11-30 08:26:22 -0700

    Update support.py

--- support.py
+++ support.py
@@ -3,9 +3,8 @@ from os import walk
 def import_folder(path):
     surface_list = []
     
-    for folder in walk(path):
-        print(folder)
+    for _,_, img_files in walk(path):
+        print(img_files)
     
     
     return surface_list
-
