77acd49acbcf2ef43ca8afdd6096901b90a09605
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-11-30 10:11:47 -0700

    Update support.py

--- support.py
+++ support.py
@@ -1,14 +1,14 @@
-from os import walk#lets us walk through different folders
+from os import walk
 import pygame
 
 def import_folder(path):
     surface_list = []
-
-    for _,_, img_files in walk(path): #walk through each folder
-        for image in img_files:#walk through each file in the folder
-            full_path = path + '/'+ image#get the full path for each image
-            image_surf = pygame.image.load(full_path).convert_alpha()#create pygame surface
-            surface_list.append(image_surf)#add it to our list of surfaces
-            print(image)
-
+    
+    
+    for _,_, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+        
     return surface_list
