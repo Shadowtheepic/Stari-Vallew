9155f89031a5cee5112cbaeab04f9d87ec724738
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-12-06 08:29:43 -0700

    Update overlay.py

--- overlay.py
+++ overlay.py
@@ -7,10 +7,18 @@ class Overlay:
         #general setup
         self.display_surface= pygame.display.get_surface()
         self.player = player
-        
+                                
         #imports
         overlay_path = "../stardew-main/overlay/"
         self.tools_surf = {tool:pygame.image.load(f'{overlay_path}{tool}.png').convert_alpha() for tool in player.tools}
         self.seeds_surf = {seed:pygame.image.load(f'{overlay_path}{seed}.png').convert_alpha() for seed in player.seeds}
         print(self.tools_surf)
         print(self.seeds_surf)
+    
+    def display(self):
+        #tools
+        tool_surf = self.tools_surf[self.player.selected_tool]
+        tool_rect = tool_surf.get_rect(midbottom = OVERLAY_POSITIONS["tool"])
+        self.display_surface.blit(tool_surf,tool_rect)
+        
+        #seeds
