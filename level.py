95e8d934bc0bd024c883d13fa44fe05fc1d4dedf
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-12-06 08:47:39 -0700

    Update level.py

--- level.py
+++ level.py
@@ -6,7 +6,7 @@ from overlay import Overlay
 
 class Level:
     def __init__(self):
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
         
         
         self.setup()
@@ -19,7 +19,18 @@ class Level:
         #print("run game")
         self.display_surface = pygame.display.get_surface()
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface)
+        #self.all_sprites.draw(self.display_surface) replaced
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt)
         self.overlay.display()
+
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
 
