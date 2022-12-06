f6a4f2059c2ed59674e398696cf3ebe7d03262c6
Shadowtheepic <78552814+Shadowtheepic@users.noreply.github.com>
2022-12-06 09:36:44 -0700

    Update player.py

--- player.py
+++ player.py
@@ -14,6 +14,7 @@ class Player(pygame.sprite.Sprite):
         #da general setup
         self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
+        self.z = LAYERS["main"]
         
         #movement attributes
         self.direction = pygame.math.Vector2()
@@ -142,5 +143,6 @@ class Player(pygame.sprite.Sprite):
         self.update_timers()
         self.move(dt)
         self.animate(dt)
+
 
 
